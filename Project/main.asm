c9c95bf729dd6805891ac2fe33c8d2559ddef41b
f380cedric <f380cedric@users.noreply.github.com>
2017-04-08 15:25:26 +0200

    Code Improvements

--- Project/main.asm
+++ Project/main.asm
@@ -38,31 +38,49 @@
 .equ SCREEN_DDR		= DDRB
 .equ SCREEN_PORT	= PORTB
 .equ SCREEN_PIN		= PINB
-.equ SCREEN_DATA	= 3
+.equ SCREEN_SDI		= 3
 .equ SCREEN_CLK		= 5
+.equ SCREEN_LE		= 4
 
 ;TCNT 1 RESET VALUE
 .EQU TCNT2_RESET_480 = 223
 .EQU TCNT0_RESET_1M = 255
 
+;Define some Rd name
 
+.DEF temp=r16
+.DEF rowselect=r21
+.DEF rowoffset=r22
+
+
+;Memory
+.DSEG
+Cell: .BYTE 16; LED cell
+.CSEG
 ;--------
 ; MACROS
 ;--------
 .MACRO shiftReg
-	SBI SCREEN_PORT, SCREEN_DATA
+	SBI SCREEN_PORT, SCREEN_SDI
 	SBRS @0, @1
-	CBI SCREEN_PORT, SCREEN_DATA
+	CBI SCREEN_PORT, SCREEN_SDI
 	SBI SCREEN_PIN,SCREEN_CLK
 	SBI SCREEN_PIN,SCREEN_CLK
 .ENDMACRO
 
-.MACRO keyboardStep2
+.MACRO charToSram
+LDI temp, low(@0<<1)
+ST Y, temp
+LDI temp, high(@0<<1)
+STD Y+1, temp
+.ENDMACRO
+
+.MACRO keyboardStep2	
 	; switch in/out for rows and columns
-	LDI r17,(1<<ROW1)|(1<<ROW2)|(1<<ROW3)|(1<<ROW4)
-	LDI r19,(1<<COL1)|(1<<COL2)|(1<<COL3)|(1<<COL4)
-	OUT KEYB_PORT,r17
-	OUT KEYB_DDR,r19
+	LDI temp,(1<<ROW1)|(1<<ROW2)|(1<<ROW3)|(1<<ROW4)
+	OUT KEYB_PORT,temp
+	LDI temp,(1<<COL1)|(1<<COL2)|(1<<COL3)|(1<<COL4)
+	OUT KEYB_DDR,temp
 	NOP
 	; check which row is LOW
 	SBIS KEYB_PIN,ROW1
@@ -76,11 +94,22 @@
 	RJMP reset
 .ENDMACRO
 
+.MACRO writeColumns
+LDS ZL, @0
+LDS ZH, @0+1
+ADD ZL,rowoffset
+LPM temp, Z
+
+shiftReg temp, 0
+shiftReg temp, 1
+shiftReg temp, 2
+shiftReg temp, 3
+shiftReg temp, 4
+.ENDMACRO
 
 ;-------
 ; CODE 
 ;-------
-.CSEG
 
 .ORG 0x0000
 rjmp init
@@ -94,9 +123,9 @@ rjmp timer2_ovf
 init:
 	; Set PB3,4,5 as output
 	; Set them at LOW
-	LDI r17, (1<<3)|(1<<4)|(1<<5)
-	OUT SCREEN_PORT, r17
-	OUT SCREEN_DDR, r17
+	LDI temp, (1<<3)|(1<<4)|(1<<5)
+	OUT SCREEN_PORT, temp
+	OUT SCREEN_DDR, temp
 
 	; configure LEDs as outputs - set to HIGH to be off
 	SBI LED_DDR,LEDUP_P
@@ -104,55 +133,90 @@ init:
 	SBI LED_PORT,LEDUP_P
 	SBI LED_PORT,LEDDOWN_P
 
+	;Clear Mem
+	LDI YH, high(Cell)
+	LDI YL, low(Cell)
+
+	LDI temp, low(CharacterEmpty<<1)
+	ST Y, temp
+	STD Y+2, temp
+	STD Y+4, temp
+	STD Y+6, temp
+	STD Y+8, temp
+	STD Y+10, temp
+	STD Y+12, temp
+	STD Y+14, temp
+	STD Y+16, temp
+	STD Y+18, temp
+	STD Y+20, temp
+	STD Y+22, temp
+	STD Y+24, temp
+	STD Y+26, temp
+	STD Y+28, temp
+	STD Y+30, temp
+	LDI temp, high(CharacterEmpty<<1)
+	STD Y+1, temp
+	STD Y+3, temp
+	STD Y+5, temp
+	STD Y+7, temp
+	STD Y+9, temp
+	STD Y+11, temp
+	STD Y+13, temp
+	STD Y+15, temp
+	STD Y+17, temp
+	STD Y+19, temp
+	STD Y+21, temp
+	STD Y+23, temp
+	STD Y+25, temp
+	STD Y+27, temp
+	STD Y+29, temp
+	STD Y+31, temp
+
+
 	; configure timer 1 in normal mode (count clk signals)
 	; WGM20 = 0   WGM21 = 0      
 	; p155 datasheet
-	LDS r16,TCCR2A
-	CBR r16,(1<<WGM20)|(1<<WGM21)
-	STS TCCR2A,r16
+	LDS temp,TCCR2A
+	CBR temp,(1<<WGM20)|(1<<WGM21)
+	STS TCCR2A,temp
 	
 	; configure prescaler to 1024
 	; CS12=0 CS11=0 CS10=1
-	LDS r16,TCCR2B
-	CBR r16,(1<<WGM22)
-	SBR r16,(1<<CS22)|(1<<CS21)|(1<<CS20)
-	STS TCCR2B,r16
+	LDS temp,TCCR2B
+	CBR temp,(1<<WGM22)
+	SBR temp,(1<<CS22)|(1<<CS21)|(1<<CS20)
+	STS TCCR2B,temp
 	
 	; activate overflow interrupt timer 1
 	; set TOIE12
-	LDS r16,TIMSK2
-	SBR r16,(1<<TOIE2)
-	STS TIMSK2,r16
+	LDS temp,TIMSK2
+	SBR temp,(1<<TOIE2)
+	STS TIMSK2,temp
 
 	; configure timer0 in normal mode
-	LDS r16,TCCR0A
-	CBR r16,(1<<WGM00)|(1<<WGM01)
-	STS TCCR0A,r16
+	LDS temp,TCCR0A
+	CBR temp,(1<<WGM00)|(1<<WGM01)
+	STS TCCR0A,temp
 
 	; set prescaler to 0 (disable) timer0
-	LDS r16,TCCR0B
-	CBR r16,(1<<WGM02)
-	CBR r16,(1<<CS02)|(1<<CS01)|(1<<CS00)
-	STS TCCR0B,r16
+	LDS temp,TCCR0B
+	CBR temp,(1<<WGM02)
+	CBR temp,(1<<CS02)|(1<<CS01)|(1<<CS00)
+	STS TCCR0B,temp
 
 	; activate overflow interrupt timer 0
-	LDS r16,TIMSK0
-	SBR r16,(1<<TOIE0)
-	STS TIMSK0,r16
+	LDS temp,TIMSK0
+	SBR temp,(1<<TOIE0)
+	STS TIMSK0,temp
 
-	LDI r22, 7
-	LDI r21, 0b1000000
 	
+	;Row offset + Row select
+	LDI rowoffset, 6
+	LDI rowselect, 1<<6
+	
+
 	SEI
-	
 
-	LDI YH, high(0x0100)
-	LDI YL, low(0x0100)
-
-	LDI r18, low(Character7<<1)
-	ST Y, r18
-	LDI r18, high(Character7<<1)
-	STD Y+1, r18
 	CLT
 	RJMP main
 
@@ -160,10 +224,10 @@ init:
 main:
 	; Check if all COL are HIGH
 		; First set all rows to LOW as output and cols as inputs
-		LDI r17,(1<<COL1)|(1<<COL2)|(1<<COL3)|(1<<COL4)
-		LDI r19,(1<<ROW1)|(1<<ROW2)|(1<<ROW3)|(1<<ROW4)
-		OUT KEYB_PORT,r17
-		OUT KEYB_DDR,r19
+		LDI temp,(1<<COL1)|(1<<COL2)|(1<<COL3)|(1<<COL4)
+		OUT KEYB_PORT,temp
+		LDI temp,(1<<ROW1)|(1<<ROW2)|(1<<ROW3)|(1<<ROW4)
+		OUT KEYB_DDR,temp
 		NOP
 		; COL1 is LOW => button 11 pressed
 		SBIS KEYB_PIN,COL1
@@ -200,161 +264,115 @@ main:
 
 		C1R1Pressed:
 			; 7 pressed -> 
-			LDI r18, low(Character7<<1)
-			ST Y, r18
-			LDI r18, high(Character7<<1)
-			STD Y+1, r18
+			charToSram Character7
 			SET
 			RJMP main
 
 		C1R2Pressed:
 			; 4 pressed -> 
-			LDI r18, low(Character4<<1)
-			ST Y, r18
-			LDI r18, high(Character4<<1)
-			STD Y+1, r18
+			charToSram Character4
 			SET
 			RJMP main
 
 		C1R3Pressed:
 			; 1 pressed -> 
-			LDI r18, low(Character1<<1)
-			ST Y, r18
-			LDI r18, high(Character1<<1)
-			STD Y+1, r18
+			charToSram Character1
 			SET
 			RJMP main
 
 		C1R4Pressed:
 			; A pressed -> 
-			LDI r18, low(CharacterA<<1)
-			ST Y, r18
-			LDI r18, high(CharacterA<<1)
-			STD Y+1, r18
+			charToSram CharacterA
 			SET
 			RJMP main
 
 		C2R1Pressed:
 			; 8 pressed -> 
-			LDI r18, low(Character8<<1)
-			ST Y, r18
-			LDI r18, high(Character8<<1)
-			STD Y+1, r18
+			charToSram Character8
 			SET
 			RJMP main
 
 		C2R2Pressed:
 			; 5 pressed -> 
-			LDI r18, low(Character5<<1)
-			ST Y, r18
-			LDI r18, high(Character5<<1)
-			STD Y+1, r18
+			charToSram Character5
 			SET
 			RJMP main
 
 		C2R3Pressed:
 			; 2 pressed -> 
-			LDI r18, low(Character2<<1)
-			ST Y, r18
-			LDI r18, high(Character2<<1)
-			STD Y+1, r18
+			charToSram Character2
 			SET
 			RJMP main
 
 		C2R4Pressed:
 			; 0 pressed -> 
-			LDI r18, low(Character0<<1)
-			ST Y, r18
-			LDI r18, high(Character0<<1)
-			STD Y+1, r18
+			charToSram Character2
 			SET
 			RJMP main
 
 		C3R1Pressed:
 			; 9 pressed -> 
-			LDI r18, low(Character9<<1)
-			ST Y, r18
-			LDI r18, high(Character9<<1)
-			STD Y+1, r18
+			charToSram Character9
 			SET
 			RJMP main
 
 		C3R2Pressed:
 			; 6 pressed -> 
-			LDI r18, low(Character6<<1)
-			ST Y, r18
-			LDI r18, high(Character6<<1)
-			STD Y+1, r18
+			charToSram Character6
 			SET
 			RJMP main
 
 		C3R3Pressed:
 			; 3 pressed -> 
-			LDI r18, low(Character3<<1)
-			ST Y, r18
-			LDI r18, high(Character3<<1)
-			STD Y+1, r18
+			charToSram Character3
 			SET
 			RJMP main
 
 		C3R4Pressed:
 			; B pressed -> 
-			LDI r18, low(CharacterB<<1)
-			ST Y, r18
-			LDI r18, high(CharacterB<<1)
-			STD Y+1, r18
+			charToSram CharacterB
 			SET
 			RJMP main
 
 		C4R1Pressed:
 			; F pressed -> 
-			LDI r18, low(CharacterF<<1)
-			ST Y, r18
-			LDI r18, high(CharacterF<<1)
-			STD Y+1, r18
+			charToSram CharacterF
 			SET
 			RJMP main
 
 		C4R2Pressed:
 			; E pressed -> 
-			LDI r18, low(CharacterE<<1)
-			ST Y, r18
-			LDI r18, high(CharacterE<<1)
-			STD Y+1, r18
+			charToSram CharacterE
 			SET
 			RJMP main
 
 		C4R3Pressed:
 			; D pressed -> 
-			LDI r18, low(CharacterD<<1)
-			ST Y, r18
-			LDI r18, high(CharacterD<<1)
-			STD Y+1, r18
+			charToSram CharacterD
 			SET
 			RJMP main
 
 		C4R4Pressed:
 			; C pressed -> 
 			CBI LED_PORT,LEDUP_P
-			LDI r18, low(CharacterC<<1)
-			ST Y, r18
-			LDI r18, high(CharacterC<<1)
-			STD Y+1, r18
+			charToSram CharacterC
 			SET
 			RJMP main
 
 
 
 timer0_ovf:
-	LDI r23,TCNT0_RESET_1M
-	STS TCNT0,r23
+	PUSH temp
+	LDI temp,TCNT0_RESET_1M
+	STS TCNT0,temp
 
-	SBI SCREEN_PIN,4
+	SBI SCREEN_PIN,SCREEN_LE
 
-	LDS r23,TCCR0B
-	CBR r23,(1<<WGM02)
-	CBR r23,(1<<CS02)|(1<<CS01)|(1<<CS00)
-	STS TCCR0B,r23
+	LDS temp,TCCR0B
+	CBR temp,(1<<WGM02)
+	CBR temp,(1<<CS02)|(1<<CS01)|(1<<CS00)
+	STS TCCR0B,temp
+	POP temp
 
 	RETI
 
@@ -362,215 +380,58 @@ timer0_ovf:
 
 timer2_ovf: 
 	; interruption routine
-	LDI r16,TCNT2_RESET_480
-	STS TCNT2,r16
+	PUSH temp
+	LDI temp,TCNT2_RESET_480
+	STS TCNT2,temp
 
 	loop:
-		DEC r22
-		BRPL notreset
-		LDI r22, 6
-		notreset:
-		LDS ZL, 0x0100
-		LDS ZH, 0x0101
-		ADD ZL,r22
-		LPM r1, Z
+		writeColumns Cell
+		writeColumns Cell+2
+		writeColumns Cell+4
+		writeColumns Cell+6
+		writeColumns Cell+8
+		writeColumns Cell+10
+		writeColumns Cell+12
+		writeColumns Cell+14
+		writeColumns Cell+16
+		writeColumns Cell+18
+		writeColumns Cell+20
+		writeColumns Cell+22
+		writeColumns Cell+24
+		writeColumns Cell+26
+		writeColumns Cell+28
+		writeColumns Cell+30
 
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-		
-		LDS ZL, 0x0102
-		LDS ZH, 0x0103
-		ADD ZL,r22
-		LPM r1, Z
 
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0104
-		LDS ZH, 0x0105
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0106
-		LDS ZH, 0x0107
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0108
-		LDS ZH, 0x0109
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0110
-		LDS ZH, 0x0111
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0112
-		LDS ZH, 0x0113
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0114
-		LDS ZH, 0x0115
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0116
-		LDS ZH, 0x0117
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0118
-		LDS ZH, 0x0119
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0120
-		LDS ZH, 0x0121
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0122
-		LDS ZH, 0x0123
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0124
-		LDS ZH, 0x0125
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0126
-		LDS ZH, 0x0127
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0128
-		LDS ZH, 0x0129
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		LDS ZL, 0x0130
-		LDS ZH, 0x0131
-		ADD ZL,r22
-		LPM r1, Z
-
-		shiftReg r1, 0
-		shiftReg r1, 1
-		shiftReg r1, 2
-		shiftReg r1, 3
-		shiftReg r1, 4
-
-		CBI SCREEN_PORT,SCREEN_DATA
+		CBI SCREEN_PORT,SCREEN_SDI
 		SBI SCREEN_PIN,SCREEN_CLK
 		SBI SCREEN_PIN,SCREEN_CLK
 
-		shiftReg r21, 6
-		shiftReg r21, 5
-		shiftReg r21, 4
-		shiftReg r21, 3
-		shiftReg r21, 2
-		shiftReg r21, 1
-		shiftReg r21, 0
-		LSR r21
+		shiftReg rowselect, 6
+		shiftReg rowselect, 5
+		shiftReg rowselect, 4
+		shiftReg rowselect, 3
+		shiftReg rowselect, 2
+		shiftReg rowselect, 1
+		shiftReg rowselect, 0
+		DEC rowoffset
+		LSR rowselect
 		BRNE end
-		LDI r21, 0b1000000
-
+		LDI rowselect, 1<<6
+		LDI rowoffset, 6
 
 		; set PB4 HIGH, wait 100 µs
 		end:
-			SBI SCREEN_PIN,4
-			LDS r16,TCCR2B
-			SBR r16,(1<<CS02)|(1<<CS01)|(1<<CS00)
-			STS TCCR0B,r16
+			SBI SCREEN_PIN,SCREEN_LE
+			LDS temp,TCCR2B
+			SBR temp,(1<<CS02)|(1<<CS01)|(1<<CS00)
+			STS TCCR0B,temp
+			POP temp
 			RETI
 
 .ORG 0x800
+CharacterEmpty:
+	.db 0, 0, 0, 0, 0, 0, 0, 0
 
 CharacterA: 
 	.db 0b00110, 0b01001, 0b01001, 0b01001, 0b01111, 0b01001, 0b01001, 0
